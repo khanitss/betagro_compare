67ccf59dcc9f48af8cc48b874ec9bc3350afe663
nathapong <nathapong.ta@gmail.com>
2017-06-10 17:55:09 +0700

    edit food

--- application/modules/food/controllers/Food.php
+++ application/modules/food/controllers/Food.php
@@ -6,15 +6,21 @@ class Food extends MX_Controller {
 	public function __construct() {
     	parent::__construct();
         $this->load->module('init_sys/Init_sys');
-        // $this->load->model('vendor/Vendor_model');
     }
 
 	public function food_page() {
-		$data['content']='food/food';
-		// print_r($data) ;
+		$data['content']='food/food-page';
 		$this->init_sys->content($data);
-		// echo "string";
 	}
 
+	public function add_food() {
+		$data['content']='food/add-food';
+		$this->init_sys->content($data);
+	}
+
+	public function food_detail() {
+		$data['content']='food/food-details';
+		$this->init_sys->content($data);
+	}
 
 }//end class
---
+++ application/modules/food/views/food-details.php
@@ -0,0 +1,137 @@
+<!-- Content Section START -->
+  <div class="block-header clearfix">
+    <h2 class="pull-left">Food Detail</h2>
+    <ul class="breadcrumb pull-right">
+      <li><a href="../../index.html">Home</a></li>
+      <li><a href="">Food</a></li>
+      <li><a href="">Food Detail</a></li>
+    </ul>
+  </div>
+
+  <!-- col standard-->
+  <div class="row">
+  <div class="col-sm-6">
+    <div class="card ">
+      <div class="card-body card-padding">
+        <h2>ขาหมูน้ำแดง</h2></br>
+        <img class="center-block" src="http://www.chiangmai-mail.com/update2014-pic/nov/17/17-11-14-n2.jpg" class="img-thumbnail" alt="Cinque Terre" style="width:75%" >
+      </div>
+      <div class="card-body card-padding">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label class="col-sm-2 col-xs-2 control-label">For :</label>
+            <div class="col-sm-10 col-xs-10">
+              <p class="form-control-static">1</p>
+            </div>
+            <label class="col-sm-2 col-xs-2 control-label">Unit :</label>
+            <div class="col-sm-10 col-xs-10">
+              <p class="form-control-static">Dish</p>
+            </div>
+            <label class="col-sm-2 col-xs-2 control-label">Time :</label>
+            <div class="col-sm-10 col-xs-10">
+              <p class="form-control-static">30 Minutes</p>
+            </div>
+            <label class="col-sm-2 col-xs-2 control-label">Total :</label>
+            <div class="col-sm-10 col-xs-10">
+              <p class="form-control-static">300 Baht</p>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="card-body card-padding">
+        <h4>รายละเอียดของเมนู Raw Material</h4>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Raw Material</th>
+              <th>Quantity</th>
+              <th>Unit</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>ขาหมู</td>
+              <td>1</td>
+              <td>ขา</td>
+              <td>300</td>
+            </tr>
+            <tr>
+              <td>คะน้า</td>
+              <td>0.5</td>
+              <td>กก.</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>ลูกกระวาน</td>
+              <td>1</td>
+              <td>กรัม</td>
+              <td>1.5</td>
+            </tr>
+            <tr>
+              <td>ไข่</td>
+              <td>1</td>
+              <td>ฟอง</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>อบเชย</td>
+              <td>2.5</td>
+              <td>กรัม</td>
+              <td>2.5</td>
+            </tr>
+            <tr>
+              <td>โปั๊ยกั๊ก</td>
+              <td>2.5</td>
+              <td>กรัม</td>
+              <td>2.5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body card-padding">
+      </br>
+      <h4>รายละเอียดของเมนู Man/Equipment</h4>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Man/Equipment</th>
+            <th>Quantity</th>
+            <th>Unit</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Chef</td>
+            <td>1</td>
+            <td>คน</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>Gas</td>
+            <td>1</td>
+            <td>Hour</td>
+            <td>400</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+</row>
+<div class="row">
+  <div class="col-sm-3">
+    <button type="button" class="btn btn-success btn-block" data-dismiss="modal">EDIT</button>
+  </div>
+  <div class="col-sm-3">
+    <a href="food.html">
+      <button type="button" class="btn btn-danger btn-block" data-dismiss="modal">BACK</button>
+    </a>
+  </div>
+  <div class="col-sm-6">q</div>
+</div>

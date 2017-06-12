c75bd5584659c78ac9cc844097a36e5b1be49243
khanitss <garcn@outlook.co.th>
2017-06-12 10:35:03 +0700

    edit image

--- application/modules/man/views/man.php
+++ application/modules/man/views/man.php
@@ -19,7 +19,7 @@
             <div class="col-md-3">
                 <div class="thumbnail img-check">
                     <center><a href= "<?php echo site_url ('man/man_edit');?>">
-                        <label for="cb1"> <img src="http://elanyeri.com/images/AdverImages/27/12041301524825.jpg"  alt="light" width="250" height="300"></lable></a>
+                        <label for="cb1"> <img src="<?php echo base_url('dist/images/menu/12041301524825.jpg');?>"  alt="light" width="250" height="300"></lable></a>
                         </center>
                         <div class="caption">
                          <center><p>Helper : 250 / hour</p></center>
@@ -28,7 +28,7 @@
              </div>
              <div class="col-md-3">
                 <div class="thumbnail img-check">
-                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmO3y5dkgWXkdKF7oOicCxQgDaPt1vn6m9rlboqRySlybQ27Hs"  alt="light" width="250" height="300"> </lable></a></center>
+                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="<?php echo base_url('dist/images/menu/images.jpg');?>"  alt="light" width="250" height="300"> </lable></a></center>
                     <div class="caption">
                         <center><p>Cheff : 300 / hour</p></center>
                     </div>
@@ -36,7 +36,7 @@
             </div>
             <div class="col-md-3">
                 <div class="thumbnail img-check">
-                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="http://img2.stockfresh.com/files/i/iodrakon/m/49/930660_91485537.jpg"  alt="light" width="250" height="300"></label></a>
+                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="<?php echo base_url('dist/images/menu/930660_91485537.jpg');?>"  alt="light" width="250" height="300"></label></a>
                     </center>
                     <div class="caption">
                         <center><p>Cheff : 250 / hour</p></center>
@@ -45,7 +45,7 @@
             </div>
             <div class="col-md-3">
                 <div class="thumbnail img-check">
-                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="http://th-live-01.slatic.net/p/3/buga-gas-refill-132ml-3-1243-5278166-7d9b6e837037c849eb6067158e0982fe-product.jpg"  alt="light" width="250" height="300"></lable></a></center>
+                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"> <img src="<?php echo base_url('dist/images/menu/gas-refill.jpg');?>"  alt="light" width="250" height="300"></lable></a></center>
                     <div class="caption">
                         <center><p>Gas : 60 / hour</p></center>
                     </div>
@@ -53,7 +53,7 @@
             </div>
             <div class="col-md-3">
                 <div class="thumbnail img-check">
-                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"><img src="https://lh5.googleusercontent.com/-F0EnuXffkxw/UPrLckZIsiI/AAAAAAAAGT0/NCzZKAwFYEE/s512/worldgas-15-kg.jpg"  alt="light" width="250" height="300"></lable></a></center>
+                    <center><a href= "<?php echo site_url ('man/man_edit');?>"><label for="cb1"><img src="<?php echo base_url('dist/images/menu/worldgas-15-kg.jpg');?>"  alt="light" width="250" height="300"></lable></a></center>
                     <div class="caption">
                         <center><p>Gas : 35 / hour</p></center>
                     </div>

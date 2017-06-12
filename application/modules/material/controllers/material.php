0317113d4ec17f41b5397cb21d4ad3aab9f53410
nathapong <nathapong.ta@gmail.com>
2017-06-12 10:49:06 +0700

    update

--- application/modules/material/controllers/material.php
+++ application/modules/material/controllers/material.php
@@ -12,8 +12,7 @@ class Material extends MX_Controller {
         $data['content']='material/add-raw-group';
         $this->init_sys->content($data);
     }
-<<<<<<< HEAD
-=======
+    
     public function material_d_page() {
         $data['content']='material/add-raw-material-page';
         $this->init_sys->content($data);
@@ -22,7 +21,5 @@ class Material extends MX_Controller {
         $data['content']='material/edit-material';
         $this->init_sys->content($data);
     }
-
->>>>>>> 532da36c09b1ee858c2c5eb05aaf44f755bfc97e
 
 }//end class

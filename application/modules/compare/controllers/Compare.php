dc955e016c3eee6154cfe224919f0d3ae28b5804
Wipada <vipada.se@gmail.com>
2017-06-13 14:11:39 +0700

    nothing

--- application/modules/compare/controllers/Compare.php
+++ application/modules/compare/controllers/Compare.php
@@ -3,11 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Compare extends MX_Controller {
 
-	public function __construct() {
+	public function __construct()
+	{
     	parent::__construct();
-        $this->load->module('init_sys/Init_sys');
-        // $this->load->model('vendor/Vendor_model');
-    }
+      $this->load->module('init_sys/Init_sys');
+      // $this->load->model('vendor/Vendor_model');
+  }
 
 	public function choose_menu1_page() {
 		$data['content']='compare/choose-menu';
---
+++ application/modules/compare/models/Compare_model.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Authen_model extends CI_Model {
+
+}//end class

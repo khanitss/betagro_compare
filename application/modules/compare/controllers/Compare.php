a283f799ddffd3312b13fa9a2dcabdcbd9fbc5a5
Wipada <vipada.se@gmail.com>
2017-06-13 15:00:43 +0700

    add food_id to session

--- application/modules/compare/controllers/Compare.php
+++ application/modules/compare/controllers/Compare.php
@@ -14,13 +14,21 @@ class Compare extends MX_Controller {
 	{
 		$qstr=array('food_type'=>0);
 		$results = $this->Compare_model->get_food_list($qstr);
-		// echo '<pre>', print_r($results);exit();
+		$data['food_standard_list'] = $results;
 
+		// echo '<pre>', print_r($results);exit();
 		$data['content']='compare/choose-menu';
 		$this->init_sys->content($data);
 	}
 
+	private function set_standard_food_id()
+	{
+		$id=($this->uri->segment(3)!=''? $this->uri->segment(3) : 0);
+		$this->session->set_userdata('selected_food_standard_id', $id);
+	}
+
 	public function choose_menu2_page() {
+		$this->set_standard_food_id();
 		$data['content']='compare/choose-menu-betagro';
 		// print_r($data) ;
 		$this->init_sys->content($data);
@@ -28,6 +36,8 @@ class Compare extends MX_Controller {
 	}
 
 	public function compare_page() {
+		print_r($this->session->userdata());exit();
+		
 		$data['content']='compare/compare';
 		// print_r($data) ;
 		$this->init_sys->content($data);
--- application/modules/compare/views/choose-menu.php
+++ application/modules/compare/views/choose-menu.php
@@ -1,108 +1,29 @@
-
 <!-- Content Section START -->
 <div class="block-header clearfix">
   <h2 class="pull-left">Choose Standard Menu</h2>
   <ul class="breadcrumb pull-right">
-                </a>
     <li><a href="<?php echo site_url('dashboard/home');?>">Home</a></li>
     <li><a href="<?php echo site_url('compare/choose_menu1_page');?>">Choose Standard Menu</a></li>
   </ul>
 </div>
-<!-- back -->
+
+<div class="row">
+  <?php foreach ($food_standard_list as $key => $value) {?>
+    <div class="col-md-4">
+      <a href="<?php echo site_url('compare/choose_menu2_page/'.$value['food_id']);?>">
         <div class="row">
-            <div class="col-sm-10 col-xs-8"></div>
-                <div class="col-sm-2 col-xs-4 text-center">
-                    <a href="<?php echo site_url('compare/choose_menu2_page');?>">
-                        <button class="btn btn-success">Next</button>
-                    </a>
-                </div>
+          <img src="<?php echo base_url('file/items_img/008.jpg');?>" class="img-responsive" />
         </div>
-<!-- choose menu -->
-            <div class="row">
-                <form method="get">
-                    <div class="form-group">
 
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item1" value="1" />
-                                    <label for="item1"> <img src="<?php echo base_url('dist/images/menu/s_ขาหมู.jpg');?>"  alt="light" style="width:90%" >
-                                        <div class="caption">ขาหมูน้ำแดง</div>
-                                </lable>
-                            </div>
-                        </div>
+        <div class="row text-center" style="margin-top: 10px;">
+          <strong><?php echo $value['food_name'];?></strong>
+        </div>
+      </a>
+    </div>
+  <?php }?>
+</div>
 
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item2" value="2" />
-                                    <label for="item2"><img src="<?php echo base_url('dist/images/menu/s_เป็ดน้ำแดง.jpg');?>" alt="Nature" style="width:90%">
-                                        <div class="caption">เป็ดน้ำแดง</div>
-                                </lable>
-                            </div>
-                        </div>
+<!-- <label for="item1">
 
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class="btn btn-light">
-                                   <input type="radio" name="rad" id="item3" value="3" />
-                                   <label for="item3"><img src="<?php echo base_url('dist/images/menu/s_ซี่โครงหมูตุ่นเยิ่อไผ่.jpg');?>" alt="Nature" style="width:90%">
-                                       <div class="caption">ซี่โครงหมูตุ๋นเยื่อไผ่</div>
-                                   </label>
-                               </lable>
-                           </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item4" value="4" />
-                                    <label for="item4"><img src="<?php echo base_url('dist/images/menu/s_ไก่ต้มน้ำปลา.jpg');?>" alt="Nature" style="width:90%">
-                                        <div class="caption">ไก่ต้มน้ำปลา</div>
-                                </lable>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item5" value="5" />
-                                    <label for="item5"><img src="<?php echo base_url('dist/images/menu/s_สเต็กเนื้อ.jpg');?>" alt="Nature" style="width:90%">
-                                        <div class="caption">สเต็กเนื้อ</div>
-                                </lable>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item6" value="6" />
-                                    <label for="item6"><img src="<?php echo base_url('dist/images/menu/s_แกงเขียวหวาน.png');?>" alt="Fjords" style="width:90%">
-                                       <div class="caption">แกงเขียวหวาน</div>
-                                </lable>
-                           </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item7" value="7" />
-                                    <label for="item7"><img src="<?php echo base_url('dist/images/menu/s_สปาเก็ตตี้คาโบนาร่า.jpg');?>" alt="Nature" style="width:90%">   
-                                        <div class="caption">สปาเก็ตตี้คาโบนาร่า</div>
-                                    </lable>
-                                </lable>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="thumbnail img-check">
-                                <label class=" btn btn-light">
-                                    <input type="radio" name="rad" id="item8" value="8" />
-                                    <label for="item8"><img src="<?php echo base_url('dist/images/menu/s_ต้มยำกุ้ง.jpg');?>"" alt="Nature" style="width:90%">
-                                        <div class="caption">ต้มยำกุ้ง</div>
-                                </lable>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+  <div class="caption">ขาหมูน้ำแดง</div>
+</lable> -->

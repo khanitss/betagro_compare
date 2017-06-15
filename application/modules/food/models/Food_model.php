7e58762401a917be1a560c474cdb078b761395cf
nathapong <nathapong.ta@gmail.com>
2017-06-15 10:03:29 +0700

    upload pic

--- application/modules/food/models/Food_model.php
+++ application/modules/food/models/Food_model.php
@@ -9,12 +9,23 @@ class Food_model extends CI_Model{
     }
 
     public function set_food_menu(){
+        $this->load->module('upload/Myupload');
+        $prop = array(
+    				'upload_path'	=>'./images_compare/',
+    				'allowed_types'	=>'jpg|jpeg|png',
+    				'txt_upload'	=>'upload_file',
+    				'txt_unlink'	=>$this->input->post('file_old'),
+    				'default_file'	=>'no-image.png'
+                );
+        $food_pic = $this->myupload->upload_file($prop);
+
         $input = array(	'food_name' 	=> $this->input->post('food_name'),
 						'food_type'		=> $this->input->post('food_type'),
 						'food_for'		=> $this->input->post('food_for'),
 						'food_unit'		=> $this->input->post('food_unit'),
 						'food_time'		=> $this->input->post('food_time'),
 						'food_status'	=> '1',
+                        'food_pic'      => $food_pic,
 						'created' 		=> $timestam,
 						'lastupdate'	=> $timestam
                     );
@@ -32,6 +43,11 @@ class Food_model extends CI_Model{
 
     public function get_man(){
         $result = $this->db->get('equipment')->result_array();
+        return $result;
+    }
+
+    public function get_mat(){
+        $result = $this->db->get('material')->result_array();
         return $result;
     }
 

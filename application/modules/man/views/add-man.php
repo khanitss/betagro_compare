876be7b24ef74e864199c84c9b7d94019c05bbb2
khanitss <garcn@outlook.co.th>
2017-06-21 11:55:53 +0700

    ใส่ป็อบอัพยืนยัน

--- application/modules/man/views/add-man.php
+++ application/modules/man/views/add-man.php
@@ -41,12 +41,37 @@
                         </div>
                         <div class="col-sm-2"></div>
                         <div class="col-sm-4">
-                            <button type="save" class="btn btn-block btn-success">บันทึก</button>
+                            <a class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myAddRawModal">บันทึก</a>
                         </div>
                         <div class="col-sm-4">
-                           <a href="<?php echo site_url ('man/man_page');?>" type="cancel" class="btn btn-danger btn-block">ยกเลิก</a>
+                           <a href="<?php echo site_url ('man/man_page');?>" type="cancel" class="btn btn-danger btn-lg btn-block">ยกเลิก</a>
                         </div>
                     </form>
+                </div>
+                <div id="myAddRawModal" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">ยืนยันข้อมูล?</h4>
+                                </div>
+
+                                <div class="modal-body">
+                                    <div class="col-sm-4"></div>
+                                    <div class="col-sm-4">
+                                        <button type="submit" class="btn btn-success btn-lg btn-block">ยืนยัน</button>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <a type="button" data-dismiss="modal" class="btn btn-danger btn-lg btn-block">ยกเลิก</a>
+                                    </div>
+                                </div>
+
+                                <div class="modal-footer">
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
